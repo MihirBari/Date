532fef3bbedf65cdac07d1bd887cdd051634ccb2
Mihir Bari <mihir.b@techsa.net>
2024-01-18 01:17:55 +0530

    Ok

--- src/home.js
+++ src/home.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import logo from './assets/img/sweet-zahraa-cute-zahraa.gif';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const [noButtonPosition, setNoButtonPosition] = useState({ left: 0, top: 0 });
+
+  const handleNoButtonClick = () => {
+    // Move the "No!!!" button to a random position
+    const left = Math.random() * (window.innerWidth - 200); // Adjust 200 based on the button width
+    const top = Math.random() * (window.innerHeight - 50); // Adjust 50 based on the button height
+    setNoButtonPosition({ left, top });
+  };
     return (
         <div className="App">
         <header className="App-header">
@@ -21,11 +29,13 @@ const Home = () => {
             </span>
         </button>
         </Link>
-        <button style={{marginLeft:"20px"}} className="custom-button" disabled>
-            <span>
-            No!!!
-            </span>
-        </button>
+        <button
+        style={{ marginLeft: "20px", position: "absolute", ...noButtonPosition }}
+        className="custom-button1"
+        onClick={handleNoButtonClick}
+      >
+        <span>No!!!</span>
+      </button>
       </div>
       );
     
